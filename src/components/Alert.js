d4d356f4c8d873d051eaa5db1422119f4be8cf3b
Abhishek Saraswat <77572876+Abhisheksaraswat21@users.noreply.github.com>
2021-07-31 09:22:11 +0530

    Update Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,9 +1,8 @@
 import React from "react";
 
 export const Alert = ({ type, text }) => {
-  return <div className={`alert alert-${type}`}>{text}</div>; //css me alert-success and alert-danger ke naam se classname hai jisme alag
-  //alag colours hai to isiliye humne yha pe paas kia hai unhe
-  // ye smjh ni aaya zyaada mujhe
+  return <div className={`alert alert-${type}`}>{text}</div>;
+ 
 };
 
 export default Alert;
