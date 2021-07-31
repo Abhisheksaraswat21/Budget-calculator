fa48f37b673bc49bf06ff03124c851e19f0736dc
Abhishek Saraswat <77572876+Abhisheksaraswat21@users.noreply.github.com>
2021-07-31 09:24:29 +0530

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,7 +2,6 @@ import React from "react";
 import { MdEdit, MdDelete } from "react-icons/md";
 
 export const ExpenseItem = ({ expense, handleEdit, handleDelete }) => {
-  //  we could have done it like expense: {id, whatever}
 
   const { id, charge, amount } = expense;
 
@@ -22,10 +21,7 @@ export const ExpenseItem = ({ expense, handleEdit, handleDelete }) => {
           onClick={() => handleEdit(id)}
         >
           <MdEdit />
-          {/* this is the button taken from the react 
-          in app.js where we have declared the handleEdit , we want an id ther taaki pta chle konsa wala delete krna hai...
-          aur id yha se paas krne ke liye arrow funciton leke kia agr,....direct handleEdit krte to uskaa koi sens enin banta hai
-          ....as a return from the arrow funtion we willl get our handler */}
+         
         </button>
 
         <button
